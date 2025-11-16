5a34c0e163d398162d8c58d4deabd5bf4cd37661
Arazm1 <araz.muhamm3d@gmail.com>
2025-11-16 21:52:13 +0200

    Finished chapter React - Hooks

--- Week_4/wsk-example-settingup-react/vite.config.js
+++ Week_4/wsk-example-settingup-react/vite.config.js
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
-  //base: '/~arazm/Web-sovelluskehitys_TX00EY23-3009_träöåpolkijgfd/Week_4/wsk_routing/wsk-example-settingup-react/',
+  base: '/~arazm/Web-sovelluskehitys_TX00EY23-3009_träöåpolkijgfd/Week_4/wsk_hooks/wsk-example-settingup-react/',
 
 })
