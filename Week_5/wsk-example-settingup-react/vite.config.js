02e833aaf526a9cb8a79d509fcce546f3e0317ff
Arazm1 <araz.muhamm3d@gmail.com>
2025-11-26 20:44:26 +0200

    Fixed path in vite.config.js

--- Week_5/wsk-example-settingup-react/vite.config.js
+++ Week_5/wsk-example-settingup-react/vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '/~arazm/Web-sovelluskehitys_TX00EY23-3009_träöåpolkijgfd/Week_5/wsk_file_upload/wsk-example-settingup-react/',
 
 })
