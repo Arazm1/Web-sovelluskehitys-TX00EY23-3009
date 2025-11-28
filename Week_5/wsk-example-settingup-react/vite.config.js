d34a9798c760cc0b4b579f6967a1fafc8b0976c9
Arazm1 <araz.muhamm3d@gmail.com>
2025-11-28 09:14:43 +0200

    vite config fix

--- Week_5/wsk-example-settingup-react/vite.config.js
+++ Week_5/wsk-example-settingup-react/vite.config.js
@@ -3,7 +3,8 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/~arazm/Web-sovelluskehitys_TX00EY23-3009_träöåpolkijgfd/Week_5/wsk_file_upload/wsk-example-settingup-react/',
-
+  plugins: [
+    react(),
+  ],
+   base: '/',
 })
